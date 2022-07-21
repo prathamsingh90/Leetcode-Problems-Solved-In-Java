d93235edf7a1472c56298783f668494e626f7440
Pratham Singh <pratham.gate2015@gmail.com>
2022-07-21 23:41:22 +0530

    Time: 1 ms (100.00%), Space: 47.6 MB (62.97%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -4,18 +4,11 @@ class Solution {
         
         for(int i=0; i<nums.length; i++){
             if (nums[i]!=nums[pos]) {
-                swap(nums, i, pos+1);
-                pos++;
-                System.out.println(nums[pos]);
+                nums[++pos]=nums[i];
             }
         }
         
         return pos+1;
     }
     
-    public static void swap(int[] arr, int i, int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
 }
